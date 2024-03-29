9c97ee77fee6d255d065dc7d0f4a68b190bb5800
Charis Giannopoulos <charilaos.giannopoulos@acg.edu>
2024-03-29 22:08:31 +0200

    Added urls.py in the catalog app, created vies for all the app's models made templates directory, created base.html and home.html templates.

---
+++ rock_merch_shop/catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('categories/', views.category_list, name='category_list'),
+    path('items/', views.item_list, name='item_list'),
+    path('item/<int:item_id>/', views.item_detail, name='item_detail'),
+    path('search/', views.search_results, name='search_results'),
+    path('band/<int:band_id>/', views.band_detail, name='band_detail'),
+]
--- rock_merch_shop/catalog/views.py
+++ rock_merch_shop/catalog/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from .models import Category, Item, Band
+
 
 # Create your views here.
+
+# Home Page View
+def home(request):
+    featured_items = Item.objects.all()[:5]
+    categories = Category.objects.all()
+    context = {
+        'featured_items': featured_items,
+        'categories': categories
+    }
+    return render(request, 'catalog/home.html', context)
+
+# Category List View
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'catalog/category_list.html', {'categories': categories})
+
+
+
+# Item List View
+def item_list(request, category_id=None):
+    if category_id:
+        items = Item.objects.filter(category=category_id)
+    else:
+        items = Item.objects.all()
+    return render(request, 'catalog/item_list.html', {'items': items})
+
+# Item Detail View
+def item_detail(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    return render(request, 'catalog/item_detail.html', {'item': item})
+
+# Search Results View
+def search_results(request):
+    query = request.GET.get('q', '')
+    items = Item.objects.filter(name__icontains=query) # Case-insensitive containment search
+    return render(request, 'catalog/search_results.html', {'items': items, 'query': query})
+
+# Band Detail View
+def band_detail(request, band_id):
+    band = get_object_or_404(Band, id=band_id)
+    items = Item.objects.filter(band=band)
+    return render(request, 'catalog/band_detail.html', {'band': band, 'items': items})
