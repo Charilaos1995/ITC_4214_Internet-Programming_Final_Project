5fee733871b076bb8ab3b3f38a35c80b9788915e
Charis Giannopoulos <charilaos.giannopoulos@acg.edu>
2024-03-29 21:20:58 +0200

    Created models for items(merchandise), categories and Bands

--- rock_merch_shop/catalog/models.py
+++ rock_merch_shop/catalog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Band(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    genre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='item_images/', blank=True,null=True)
+
+    def __str__(self):
+        return self.name
