bb6671649e3dd7944b5cc9d38ef98c9de40e1b88
Charis Giannopoulos <charilaos.giannopoulos@acg.edu>
2024-04-03 15:33:34 +0300

    Added items in Featured Items section, new categories and styling.

--- rock_merch_shop/catalog/urls.py
+++ rock_merch_shop/catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('categories/', views.category_list, name='category_list'),
-    path('items/', views.item_list, name='item_list'),
+    path('items/<int:category_id>/', views.item_list, name='item_list'),
     path('item/<int:item_id>/', views.item_detail, name='item_detail'),
     path('search/', views.search_results, name='search_results'),
     path('band/<int:band_id>/', views.band_detail, name='band_detail'),
